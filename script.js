698e79f7e88073723e29559892da8b6ff37f80fe
Dipayan Murmu <dipayanmurmu8@gmail.com>
2021-05-24 10:10:40 +0530

    Final

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-function compute()
+function output()
+        {
+        var principal=parseFloat(document.getElementById("Pr").value);
+        var rate=parseFloat(document.getElementById("Rt").value);
+        var time=parseFloat(document.getElementById("T").value);
+        amount=((principal*rate*time)/100)+principal;
+        result=document.getElementById("amt");
+        result.innerHTML="Amount: "+amount;
+        }
+function refresh()
 {
-    p = document.getElementById("principal").value;
-    
+    result=document.getElementById("amt");
+        result.innerHTML="Amount: ";
 }
-        
