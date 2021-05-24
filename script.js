1afbea5c6ae0dc27f8dba77a374ddf403a144396
dipayan-98 <60866010+dipayan-98@users.noreply.github.com>
2021-05-24 10:18:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function output()
         var principal=parseFloat(document.getElementById("Pr").value);
         var rate=parseFloat(document.getElementById("Rt").value);
         var time=parseFloat(document.getElementById("T").value);
-        interest=((principal*rate*time)/100)+interest;
+        interest=((principal*rate*time)/100);
         result=document.getElementById("amt");
-        result.innerHTML="Interest: "+amount;
+        result.innerHTML="Interest: "+interest;
         }
 function refresh()
 {
